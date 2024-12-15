df8d1ba8d8b024abf64cae4c06cccf0d5a0282b4
andreysavart <andrejsavkin751@gmail.com>
2024-12-15 13:25:20 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Body, HTTPException, Path
-from typing import List, Annotated
+from fastapi import FastAPI, Path, status, Body, HTTPException
+from typing import Annotated, List
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,36 +13,37 @@ class User(BaseModel):
     age: int
 
 
-@app.get("/users")
-async def get_users() -> List[User]:
+@app.get('/users')
+def get_users() -> List[User]:
     return users
 
 
-@app.post("/user/{username}/{age}")
-async def post_user(
-        username: Annotated[str, Path(min_length=4, max_length=20, description='Enter username', example="Alex")],
-        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="40")]
-) -> str:
+@app.post('/user/{username}/{age}')
+def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
     new_id = (users[-1].id + 1) if users else 1
     new_user = User(id=new_id, username=username, age=age)
     users.append(new_user)
-    return f"id: {new_user.id}, username: {new_user.username}, age: {new_user.age}"
+    return new_user
 
 
-@app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, new_username: str = Body(), new_age: int = Body()) -> str:
+@app.put('/user/{user_id}/{username}/{age}')
+def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
     for user in users:
         if user.id == user_id:
-            user.username = new_username
-            user.age = new_age
-            return f"id: {user.id}, username: {user.username}, age: {user.age}"
-    raise HTTPException(status_code=404, detail="User was not found")
+            user.username = username
+            user.age = age
+            return user
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
 
 
-@app.delete("/user/{user_id}")
-async def delete_user(user_id: int) -> str:
-    for index, user in enumerate(users):
+@app.delete('/user/{user_id}')
+def delite_user(user_id: int) -> str:
+    for i, user in enumerate(users):
         if user.id == user_id:
-            user_del = users.pop(index)
-            return f"id: {user_del.id}, username: {user_del.username}, age: {user_del.age}"
-    raise HTTPException(status_code=404, detail="User was not found")
+            return users.pop(i)
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
